340f49fa65dd2620b340aeb6d322364f0be05882
Radek <radek.dumin@gmail.com>
2026-03-03 10:57:31 +0100

    layout fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <main className="flex-1">
-        <section className="container mx-auto- mx-4 my-32">
+        <section className="container mx-auto  my-32">
           <div className="mx-auto max-w-4xl text-center">
             <h1 className="text-black text-6xl mb-6 font-bold">
               A better way to track your application
